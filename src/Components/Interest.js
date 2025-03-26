a208764897710a88fe815287831543b547f1dde3
Jogeshwaran <64849330+Jogeshwaran@users.noreply.github.com>
2025-03-27 00:42:12 +0530

    Update Interest.js

--- src/Components/Interest.js
+++ src/Components/Interest.js
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
+
 export default Interest = ({ data }) => {
   let { interest } = data;
+  const [isChecked, setIsChecked] = useState({});
+  const onChangeHandler = (e) => {
+    const { name, checked } = e;
+    setIsChecked((prevState) => ({
+      ...prevState,
+      [name]: checked,
+    }));
+  };
+
   return (
     <div>
       {interest.map((i) => (
         <ul>
-          <li>{i}</li>
+          <input
+            type="checkbox"
+            checked={isChecked.i}
+            name={i}
+            onChange={(e) => onChangeHandler(e)}
+          />
+          {i}
         </ul>
       ))}
     </div>
